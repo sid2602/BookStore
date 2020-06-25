2a2397aac6b270bc689bd87ff3d2c7f8c0c408eb
Filip Kornaus <kornaus.filip@gmail.com>
2020-06-25 18:48:14 +0200

    Updates

---
+++ precache-manifest.e7eb0c8875cac5f7ed0515427da4b4ce.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "45d077d65cae6613cca14f732c7fd94b",
+    "url": "/Bookstore/index.html"
+  },
+  {
+    "revision": "685294a0b00fff37535f",
+    "url": "/Bookstore/static/css/main.61afc369.chunk.css"
+  },
+  {
+    "revision": "56a0a90312372cbb3885",
+    "url": "/Bookstore/static/js/2.d676fd8e.chunk.js"
+  },
+  {
+    "revision": "0736aa59ebcfe49a59888a5eab5fa679",
+    "url": "/Bookstore/static/js/2.d676fd8e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "685294a0b00fff37535f",
+    "url": "/Bookstore/static/js/main.f39ade35.chunk.js"
+  },
+  {
+    "revision": "5fb3bf3b19eb21dca08a",
+    "url": "/Bookstore/static/js/runtime-main.54047c3f.js"
+  }
+]);
